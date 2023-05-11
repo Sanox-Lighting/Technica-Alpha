9e9d1c1d9c95c106289ee69540c0d11edf62c3cb
Frost <93394571+DaRealFrost@users.noreply.github.com>
2023-05-11 20:04:59 +0100

    Update Vibrate.lua
    Signed-off-by: Frost <93394571+DaRealFrost@users.noreply.github.com>

--- src/Workspace/INSIDE - Technica_Engine/Engine/Vibrate.lua
+++ src/Workspace/INSIDE - Technica_Engine/Engine/Vibrate.lua
@@ -1,3 +1,23 @@
+--[[***
+ * Technica Alpha
+ *
+ * Copyright (c) [2023(s)] [Sanox Lighting]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ ***]]--
 for i,v in pairs(game.Workspace.TaLights:GetChildren()) do
 	spawn(function()
 		v.Head.Lens.Transparency = 1 v.Head.Beam.light.Transparency = NumberSequence.new(1) v.Head.Beam.SpotLight.Brightness = 0
