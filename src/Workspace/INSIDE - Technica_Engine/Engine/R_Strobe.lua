f056aa2f6186a03fec222c8005223cb9aff99431
Frost <93394571+DaRealFrost@users.noreply.github.com>
2023-05-11 20:04:33 +0100

    Update R_Strobe.lua
    Signed-off-by: Frost <93394571+DaRealFrost@users.noreply.github.com>

--- src/Workspace/INSIDE - Technica_Engine/Engine/R_Strobe.lua
+++ src/Workspace/INSIDE - Technica_Engine/Engine/R_Strobe.lua
@@ -1,3 +1,23 @@
+--[[***
+ * Technica Alpha
+ *
+ * Copyright (c) [2023(s)] [Sanox Lighting]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ ***]]--
 for i,v in pairs (game:GetService("Workspace").TaLights:GetChildren()) do spawn(function() while wait() do
 			if v.Name == "Ta2" or v.Name == "Ta4" or v.Name == "Ta6" or v.Name == "Ta8" or v.Name == "Ta10" or v.Name == "Ta12" or v.Name == "Ta13" or v.Name == "Ta15" or v.Name == "Ta17" or v.Name == "Ta19" or v.Name == "Ta21" or v.Name == "Ta23" then
 				v.Head.Beam.light.Transparency = NumberSequence.new(0) v.Head.Beam.SpotLight.Brightness = 10 v.Head.Lens.Transparency = 0
